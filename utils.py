e3459ce6807fbad0ed8ab844fa6f9ef352404fe0
ViCoder <145913485+resources3312@users.noreply.github.com>
2025-08-29 19:22:54 +0300

    Show last commit

--- utils.py
+++ utils.py
@@ -3,29 +3,41 @@ from os.path import isfile, isdir
 from xxhash import xxh64
 
 
+
 class FileCMPManager:
-
-    @staticmethod
-    def getFileCheckSum(path: str, chunk_size=4096) -> bin:
-        try:
-            with open(path, "rb") as f:
-                func = xxh64()
-                for chunk in iter(lambda: f.read(chunk_size), b""): func.update(chunk)
-                return func.digest()
-
-        except FileNotFoundError: False 
-    
-    @staticmethod
-    def cmpFilePair(path_1: str, path_2: str) -> bool:
-        return FileCMPManager.getFileCheckSum(path_1) == FileCMPManager.getFileCheckSum(path_2) if isfile(path_1) and isfile(path_2) else False
     
     @staticmethod
     def getFilesCount(dir_path: str) -> int:
         try: return len(listdir(dir_path))
         except: return -1
     
+    @staticmethod
     def getFilenamesOfDirectory(dirPath: str) -> tuple:
         return tuple([f"{dirPath}/{filename}" for filename in listdir(dirPath)])
+    
+    @staticmethod
+    def getRawBytesCheckSum(buffer: bytes) -> bin:
+        func = xxh64()
+        func.update(buffer)
+        return func.digest()
+    
+    @staticmethod
+    def cmpByteArrayPair(buffer_1: bytes, buffer_2: bytes) -> bool:
+        return FileCMPManager.getRawBytesCheckSum(buffer_1) == FileCMPManager.getRawBytesCheckSum(buffer_2)
+    
+    @staticmethod
+    def getFileCheckSum(path: str, chunk_size=4096) -> bin:
+        try:
+            with open(path, "rb") as f:
+                return FileCMPManager.getRawBytesCheckSum(f.read())
+
+        except FileNotFoundError: return False 
+
+    @staticmethod
+    def cmpFilePair(path_1: str, path_2: str) -> bool:
+        return FileCMPManager.getFileCheckSum(path_1) == FileCMPManager.getFileCheckSum(path_2) if isfile(path_1) and isfile(path_2) else False
+    
+    
 
     @staticmethod
     def getLowDirectory(dirPath1: str, dirPath2: str) -> int:
@@ -35,11 +47,9 @@ class FileCMPManager:
             return len1 if len1 < len2 else len2
 
         except: return -1
-
+    
     @staticmethod
-    def cmpDirectoryContent(dirPath1, dirPath2) -> list:
+    def cmpDirectoryContent(dirPath1, dirPath2) -> bool:
         dirNames1 = FileCMPManager.getFilenamesOfDirectory(dirPath1)
         dirNames2 = FileCMPManager.getFilenamesOfDirectory(dirPath2)
         return any(FileCMPManager.cmpFilePair(dirNames1[i], dirNames2[i]) for i in range(FileCMPManager.getLowDirectory(dirPath1, dirPath2))) if isdir(dirPath1) and isdir(dirPath2) else False
-
-    
